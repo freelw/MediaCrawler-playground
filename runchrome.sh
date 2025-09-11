c5a0a7585740fc169f0fa5b80b764a765bcbdab1
freelw <826231693@qq.com>
2025-09-11 14:36:03 +0800

    update

--- runchrome.sh
+++ runchrome.sh
@@ -2,5 +2,7 @@
 
 docker run -it --rm \
   --network host \
+  --privileged \
+  -v /run/dbus/system_bus_socket:/run/dbus/system_bus_socket \
   wangli-chrome:latest /bin/bash
-  
+# xvfb-run /usr/bin/google-chrome-stable --no-sandbox --remote-debugging-port=9222 --user-data-dir=/tmp/chrome_dbg
